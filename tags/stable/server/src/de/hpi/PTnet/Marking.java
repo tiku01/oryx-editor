23e1490d998c85cc545e20ebcebd26963b3b9cdd
NicoPeters.NP <NicoPeters.NP@d672c736-503d-0410-a38a-9366997c882b>
2008-07-10 11:26:16 +0000

    creating new stable tag

---
+++ tags/stable/server/src/de/hpi/PTnet/Marking.java
@@ -0,0 +1,18 @@
+package de.hpi.PTnet;
+
+import java.util.List;
+
+import de.hpi.petrinet.Place;
+
+
+public interface Marking extends de.hpi.petrinet.Marking {
+	
+	void setNumTokens(Place p, int numTokens);
+	
+	void addToken(Place p);
+	
+	boolean removeToken(Place p);
+	
+	List<Place> getMarkedPlaces();
+	
+}
