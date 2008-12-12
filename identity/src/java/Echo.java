fce6b25cdc57cc9241ccc13a8fe2b351da7d4057
NicoPeters.NP <NicoPeters.NP@d672c736-503d-0410-a38a-9366997c882b>
2008-12-12 10:47:09 +0000

    identitySOA: implemented SAMLHandler for SAML authentification

--- identity/src/java/Echo.java
+++ identity/src/java/Echo.java
@@ -1,9 +1,12 @@
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringEscapeUtils;
 
 public class Echo extends HttpServlet {
 
@@ -13,14 +16,48 @@ public class Echo extends HttpServlet {
 	private static final long serialVersionUID = -4453724722118567472L;
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) {
-		String xmlToken = req.getParameter("xmlToken");
-		
 		try {
-			res.getOutputStream().println("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
-			res.getOutputStream().println(xmlToken);
+			String xmlToken = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>"
+					+ req.getParameter("xmlToken");
+			System.out.println("xmltoken= " + xmlToken);
+
+			res.setContentType("text/html");
+			res.getWriter().println(
+					this.getRedirectPage("http://localhost:8080/backend/poem/saml", xmlToken));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	protected String getRedirectPage(String url, String xmltoken) {
+		try {
+			String page = "<html xmlns=\"http://www.w3.org/1999/xhtml\">"
+					+ "<head>"
+					+ "<title>Redirection with SAML token</title>"
+					+ "</head>"
+					+ "<body>"
+					+ "<form name=\"saml-form-redirection\" action=\"" + url
+					+ "\" method=\"get\" accept-charset=\"utf-8\" >"
+					+ "<input type=\"hidden\" name=\"xmltoken\" value=\"" + StringEscapeUtils.escapeHtml(xmltoken) + "\"/>"
+					+ "<input type=\"hidden\" name=\"returnto\" value=\"/identity/newmodel.html\"/>"
+					+ "<button type=\"submit\">Login to Oryx via GET</button>" + "</form>"
+					
+					+ "<form name=\"saml-form-redirection\" action=\"" + url
+					+ "\" method=\"post\" accept-charset=\"utf-8\" >"
+					+ "<input type=\"hidden\" name=\"xmltoken\" value=\"" + StringEscapeUtils.escapeHtml(xmltoken) + "\"/>"
+					+ "<input type=\"hidden\" name=\"returnto\" value=\"/identity/newmodel.html\"/>"
+					+ "<button type=\"submit\">Login to Oryx via POST</button>" + "</form>"
+					
+					+ "<a href=\"" + url + "?xmltoken="
+					+ URLEncoder.encode(xmltoken, "UTF-8")
+					+ "&returnto=" + URLEncoder.encode("/identity/newmodel.html", "UTF-8")
+					+ "\">Login to Oryx</a>"
+					+ "</body>" + "</html>";
+
+			return page;
+		} catch (UnsupportedEncodingException e) {
+			return null;
 		}
 	}
 }
