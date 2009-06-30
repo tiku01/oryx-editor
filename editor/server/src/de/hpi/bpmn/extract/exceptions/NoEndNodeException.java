ceae28d65ebdb8a7e1cf465c52b639521f9f10fc
W.Tscheschner <W.Tscheschner@d672c736-503d-0410-a38a-9366997c882b>
2009-06-30 19:50:10 +0000

    Initial Version of extraction of core processes

---
+++ editor/server/src/de/hpi/bpmn/extract/exceptions/NoEndNodeException.java
@@ -0,0 +1,24 @@
+package de.hpi.bpmn.extract.exceptions;
+
+public class NoEndNodeException extends Exception {
+
+	public NoEndNodeException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoEndNodeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoEndNodeException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoEndNodeException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ editor/server/src/de/hpi/bpmn/extract/exceptions/NoStartNodeException.java
@@ -0,0 +1,24 @@
+package de.hpi.bpmn.extract.exceptions;
+
+public class NoStartNodeException extends Exception {
+
+	public NoStartNodeException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoStartNodeException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoStartNodeException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoStartNodeException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
