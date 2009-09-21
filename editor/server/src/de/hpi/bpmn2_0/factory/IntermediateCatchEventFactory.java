e9b1815663249e7ea33f650e86f3dbb2fbe69dfd
sven.wagnerboysen <sven.wagnerboysen@d672c736-503d-0410-a38a-9366997c882b>
2009-09-21 15:41:50 +0000

    more related to last commit

---
+++ editor/server/src/de/hpi/bpmn2_0/factory/IntermediateCatchEventFactory.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2009
+ * Philipp Giese, Sven Wagner-Boysen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package de.hpi.bpmn2_0.factory;
+
+import org.oryxeditor.server.diagram.Shape;
+
+import de.hpi.bpmn2_0.exceptions.BpmnConverterException;
+import de.hpi.bpmn2_0.factory.annotations.StencilId;
+import de.hpi.bpmn2_0.model.BaseElement;
+import de.hpi.bpmn2_0.model.activity.Activity;
+import de.hpi.bpmn2_0.model.diagram.EventShape;
+import de.hpi.bpmn2_0.model.event.BoundaryEvent;
+import de.hpi.bpmn2_0.model.event.IntermediateCatchEvent;
+import de.hpi.bpmn2_0.model.Process;
+
+/**
+ * Factory to create intermediate catching Events
+ * 
+ * @author Philipp Giese
+ * @author Sven Wagner-Boysen
+ *
+ */
+@StencilId("IntermediateTimerEvent")
+public class IntermediateCatchEventFactory extends AbstractBpmnFactory {
+
+	/* (non-Javadoc)
+	 * @see de.hpi.bpmn2_0.factory.AbstractBpmnFactory#createBpmnElement(org.oryxeditor.server.diagram.Shape, de.hpi.bpmn2_0.factory.BPMNElement)
+	 */
+	@Override
+	public BPMNElement createBpmnElement(Shape shape, BPMNElement parent)
+			throws BpmnConverterException {
+		EventShape icEventShape = (EventShape) this.createDiagramElement(shape);
+		IntermediateCatchEvent icEvent = (IntermediateCatchEvent) this.createProcessElement(shape);
+		icEventShape.setEventRef(icEvent);
+		
+		return new BPMNElement(icEventShape, icEvent, shape.getResourceId());
+	}
+
+	/* (non-Javadoc)
+	 * @see de.hpi.bpmn2_0.factory.AbstractBpmnFactory#createDiagramElement(org.oryxeditor.server.diagram.Shape)
+	 */
+	@Override
+	protected Object createDiagramElement(Shape shape) {
+		EventShape intermediateEventShape = new EventShape();
+		this.setVisualAttributes(intermediateEventShape, shape);
+		
+		return intermediateEventShape;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.hpi.bpmn2_0.factory.AbstractBpmnFactory#createProcessElement(org.oryxeditor.server.diagram.Shape)
+	 */
+	@Override
+	protected BaseElement createProcessElement(Shape shape)
+			throws BpmnConverterException {
+		IntermediateCatchEvent icEvent = new IntermediateCatchEvent();
+		icEvent.setId(shape.getResourceId());
+		icEvent.setName(shape.getProperty("name"));
+		
+		return icEvent;
+	}
+	
+	public static void changeToBoundaryEvent(BPMNElement activity, BPMNElement event, Process process) {
+		if(!(activity.getNode() instanceof Activity) || !(event.getNode() instanceof IntermediateCatchEvent)) {
+			return;
+		}
+		
+		BoundaryEvent bEvent = new BoundaryEvent();
+		bEvent.setAttachedToRef((Activity) activity.getNode());
+		bEvent.setId(event.getNode().getId());
+		bEvent.setName(((IntermediateCatchEvent) event.getNode()).getName());
+		bEvent.setParallelMultiple(((IntermediateCatchEvent) event.getNode()).isParallelMultiple());
+		// TODO: bEvent.setCancelActivity()
+		
+		/* Refresh references */
+		int index = process.getFlowElement().indexOf(event.getNode());
+		if(index != -1) {
+			process.getFlowElement().remove(index);
+			process.getFlowElement().add(index, bEvent);
+		}
+		event.setNode(bEvent);
+		((EventShape) event.getShape()).setEventRef(bEvent);
+		((Activity)activity.getNode()).getBoundaryEventRefs().add(bEvent);
+	}
+
+}
