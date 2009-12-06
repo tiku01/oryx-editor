5776d3affbaadea130a1b0d91953366459842191
gerar_1989@yahoo.de <gerar_1989@yahoo.de@d672c736-503d-0410-a38a-9366997c882b>
2009-12-06 21:10:00 +0000

    erstes plugin

--- branches/pms09-08/editor/client/scripts/Plugins/cpntokenpositioning.js
+++ branches/pms09-08/editor/client/scripts/Plugins/cpntokenpositioning.js
@@ -2,52 +2,36 @@ if (!ORYX.Plugins)
     ORYX.Plugins = new Object();
 
 ORYX.Plugins.cpntokenpositioning = Clazz.extend
-(
-	{	
-		// Defines the facade
-	    facade		: undefined,
-	    		
-		// Constructor 
-	    construct: function(facade)
-	    {
-	    
-	        this.facade = facade;     
-			
-			// Offers the functionality of positioning
-	        this.facade.offer({
-				name			: ORYX.I18N.Undo.undo,
-				description		: ORYX.I18N.Undo.undoDesc,
-				icon			: ORYX.PATH + "images/arrow_undo.png",
-				keyCodes: [{
-						metaKeys: [ORYX.CONFIG.META_KEY_META_CTRL],
-						keyCode: 90,
-						keyAction: ORYX.CONFIG.KEY_ACTION_DOWN
-					}
-			 	],
-				functionality	: this.doUndo.bind(this),
-				group			: ORYX.I18N.Undo.group,
-				isEnabled		: function(){ return this.undoStack.length > 0 }.bind(this),
-				index			: 0
-			}); 
-	
-			// Offers the functionality of redo
-	        this.facade.offer({
-				name			: ORYX.I18N.Undo.redo,
-				description		: ORYX.I18N.Undo.redoDesc,
-				icon			: ORYX.PATH + "images/arrow_redo.png",
-				keyCodes: [{
-						metaKeys: [ORYX.CONFIG.META_KEY_META_CTRL],
-						keyCode: 89,
-						keyAction: ORYX.CONFIG.KEY_ACTION_DOWN
-					}
-			 	],
-				functionality	: this.doRedo.bind(this),
-				group			: ORYX.I18N.Undo.group,
-				isEnabled		: function(){ return this.redoStack.length > 0 }.bind(this),
-				index			: 1
-			}); 
-			
-			// Register on event for executing commands --> store all commands in a stack		 
-			this.facade.registerOnEvent(ORYX.CONFIG.EVENT_EXECUTE_COMMANDS, this.handleExecuteCommands.bind(this) );
+({
+	// Defines the facade
+    facade		: undefined,
+    
+	// Constructor 
+    construct: function(facade)
+    {
+    
+        this.facade = facade;     
+		
+		// Offers the functionality of tokenpositioning                
+        this.facade.offer
+        ({
+			name			: "Token Positioning",
+			description		: "Token Positioning",
+			icon			: ORYX.PATH + "images/arrow_undo.png",
+			functionality	: this.tokenpositioning.bind(this),
+			group			: ORYX.I18N.Undo.group,
+			isEnabled		: true,
+			index			: 0
+		}); 
+		
+        // Register in order to get events when a place is resize
+		//this.facade.registerOnEvent(ORYX.CONFIG.EVENT_RESIZE_END, this.tokenpositioning.bind(this));
+    },
+    
+    tokenpositioning: function(param)
+    {
+    	alert("hjallsod");    	
+    }
+})
     	
 	
