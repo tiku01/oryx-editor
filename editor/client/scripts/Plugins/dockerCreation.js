e21b05ffcc8c0a17bf33868d5a4dad4dbc422095
mr.pe.2000 <mr.pe.2000@d672c736-503d-0410-a38a-9366997c882b>
2010-01-09 14:49:23 +0000

    circle added(with wrong position)

--- editor/client/scripts/Plugins/dockerCreation.js
+++ editor/client/scripts/Plugins/dockerCreation.js
@@ -4,7 +4,6 @@
  * 
  **/
 
-
 if(!ORYX.Plugins)
 	ORYX.Plugins = new Object();
 
@@ -13,35 +12,13 @@ ORYX.Plugins.DockerCreation = Clazz.extend({
 	//facade: null,
 	
 	construct: function( facade ){
-		this.facade = facade;
-		/*
-		this.shapeSelection = undefined;
-		this.docker 		= undefined;
-		this.dockerParent   = undefined;
-		this.dockerSource 	= undefined;
-		this.dockerTarget 	= undefined;
-		this.lastUIObj 		= undefined;
-		this.isStartDocker 	= undefined;
-		this.isEndDocker 	= undefined;
-		this.undockTreshold	= 10;
-		this.initialDockerPosition = undefined;
-		this.outerDockerNotMoved = undefined;
-		this.isValid 		= false; */
-		this.counter=0;
-		this.ghost=ORYX.Editor.graft("http://www.w3.org/2000/svg",null,['g']);
-		
-		this.ghostPoint = ORYX.Editor.graft("http://www.w3.org/2000/svg",
-				this.ghost,
-				['g', {"pointer-events":"all"},
-						['circle', {cx:"8", cy:"8", r:"8", stroke:"none", fill:"none"}],
-						['circle', {cx:"8", cy:"8", r:"3", stroke:"black", fill:"yellow", "stroke-width":"1"}]
-					]);
+		this.facade = facade;		
+		this.active = false;
 		
 		this.facade.registerOnEvent(ORYX.CONFIG.EVENT_MOUSEDOWN, this.handleMouseDown.bind(this));
 		this.facade.registerOnEvent(ORYX.CONFIG.EVENT_MOUSEOVER, this.handleMouseOver.bind(this));
 		this.facade.registerOnEvent(ORYX.CONFIG.EVENT_MOUSEOUT, this.handleMouseOut.bind(this));
-		
-		this.addEventHandlers(this.ghostPoint);
+		this.facade.registerOnEvent(ORYX.CONFIG.EVENT_MOUSEMOVE, this.handleMouseMove.bind(this));
 },
 
 /**
@@ -49,50 +26,63 @@ ORYX.Plugins.DockerCreation = Clazz.extend({
  *
  */
 
+//hide the Ghostpoint when Leaving the mouse from an edge
 handleMouseOut: function(event, uiObj) {
 	
-	//hideDocker 
-	
-/*	if (uiObj instanceof ORYX.Core.Docker){
+	if (this.active) {
 		
-		this.deleteDockerCommand({ edge: uiObj, position: this.facade.eventCoordinates(event)});
-	}*/
+		this.hideOverlay();
+		this.active=false;
+	}	
 },
 
+//show the ghostpoint
 handleMouseOver: function(event, uiObj) {
+
+	var evPos = this.facade.eventCoordinates(event);
 	
-	//show Point at MousePosition
-	
-/*	
-	if (uiObj instanceof ORYX.Core.Edge){
+	console.log("mouseoverstart");
+	if( uiObj instanceof ORYX.Core.Edge){
 		
-		this.counter+=1;
-		
-		
-	this.addDockerCommand({ edge: uiObj, position: this.facade.eventCoordinates(event)});
-		
-			
-		
-	}*/		
+		this.showOverlay( uiObj, evPos )
+	}
+	this.active=true;
 },
 
-handleMouseDown: function(event, uiObj) {
-	
-	//createDocker
+
+//create a Docker
+handleMouseDown: function(event, uiObj) {	
 	
 	if (uiObj instanceof ORYX.Core.Edge){
 		this.addDockerCommand({
             edge: uiObj,
             position: this.facade.eventCoordinates(event)
-        });
-		
-	}
-		
+        });		
+		console.log("Docker added");		
+	}		
 },
 
+//refresh the ghostpoint when moving the mouse ober an edge
+handleMouseMove: function(event, uiObj) {		
+	
+	if (uiObj instanceof ORYX.Core.Edge){
+		var evPos = this.facade.eventCoordinates(event);
+		console.log("MouseMove");
+		if (this.active) {
+			
+			this.facade.raiseEvent({
+				type: ORYX.CONFIG.EVENT_OVERLAY_HIDE,
+				id: "ghostpoint"
+			});			
+			this.showOverlay( uiObj, evPos );
+		}
+		else{
+			this.showOverlay( uiObj, evPos );	
+		}		
+	}		
+},
 
-
-
+//Command for creating a new Docker
 addDockerCommand: function(options){
     if(!options.edge)
         return;
@@ -109,8 +99,7 @@ addDockerCommand: function(options){
         execute: function(){
            
             this.docker = this.edge.addDocker(this.pos, this.docker);
-			this.index = this.edge.dockers.indexOf(this.docker);
-                                    
+			this.index = this.edge.dockers.indexOf(this.docker);                                    
             this.facade.getCanvas().update();
             this.facade.updateSelection();
         },
@@ -118,57 +107,44 @@ addDockerCommand: function(options){
           
              if (this.docker instanceof ORYX.Core.Controls.Docker) {
                     this.edge.removeDocker(this.docker);
-             } 
-            
+             }             
             this.facade.getCanvas().update();
             this.facade.updateSelection(); 
         }
-    })
-    
-    var command = new commandClass(options.edge, options.docker, options.position, this.facade);
-    
+    })    
+    var command = new commandClass(options.edge, options.docker, options.position, this.facade);    
     this.facade.executeCommands([command]);
 },
 
-deleteDockerCommand: function(options){
-    if(!options.edge)
-        return;
+//show the ghostpoint
+showOverlay: function(edge, position){
 
-    var commandClass = ORYX.Core.Command.extend({
-        construct: function(edge, docker, pos, facade){
-            
-            this.edge = edge;
-            this.docker = docker;
-            this.pos = pos;
-            this.facade = facade;
-			//this.index = docker.parent.dockers.indexOf(docker);
-        },
-        execute: function(){
-           
-        	this.index = this.edge.dockers.indexOf(this.docker);
-            this.pos = this.docker.bounds.center();
-            this.edge.removeDocker(this.docker);
-                                    
-            this.facade.getCanvas().update();
-            this.facade.updateSelection();
-        },
-        rollback: function(){
-          
-        	//this.edge.add(this.docker, this.index); 
-            
-            this.facade.getCanvas().update();
-            this.facade.updateSelection(); 
-        }
-    })
-    
-    var command = new commandClass(options.edge, options.docker, options.position, this.facade);
-    
-    this.facade.executeCommands([command]);
+	var circle = ORYX.Editor.graft("http://www.w3.org/2000/svg", null ,
+				['g', {"pointer-events":"none"},
+					['circle', {cx: "8", cy: "8", r: "3", fill:"yellow"}]]);
+						
+	console.log(position);
+	
+	this.facade.raiseEvent({
+			type: 			ORYX.CONFIG.EVENT_OVERLAY_SHOW,
+			id: 			"ghostpoint",
+			shapes: 		[edge],
+			//attributes: 	{fill: "red", stroke:"green", "stroke-width":4},
+			node:			circle,
+			nodePosition:	"northeast",
+			dontCloneNode:	true
+		});
+	console.log("event fertig");				
+},
+
+//hide the ghostpoint
+hideOverlay: function() {
+	
+	this.facade.raiseEvent({
+		type: ORYX.CONFIG.EVENT_OVERLAY_HIDE,
+		id: "ghostpoint"
+	});	
 }
-
-
-
-
 
 
 });
