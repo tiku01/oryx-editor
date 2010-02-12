a3199a6cd9cf0931494229d63b517453fd54060b
martin.krueger@student.hpi.uni-potsdam.de <martin.krueger@student.hpi.uni-potsdam.de@d672c736-503d-0410-a38a-9366997c882b>
2010-02-12 04:15:30 +0000

    fixed tabs

--- poem-jvm/src/java/org/b3mn/poem/handler/XMLFileExporter.java
+++ poem-jvm/src/java/org/b3mn/poem/handler/XMLFileExporter.java
@@ -30,40 +30,41 @@ import org.b3mn.poem.Identity;
 import org.b3mn.poem.util.ExportHandler;
 @ExportHandler(uri="/xml", formatName="XML File", iconUrl="/backend/images/silk/page_code.png")
 public class XMLFileExporter extends HandlerBase {
- final static String begin="<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-   "<oryxmodel>" +
-   "<description>";
- final static String part1="</description>" +
-   "<type>";
- final static String part2="</type>" +
-   "<json-representation><![CDATA[";
- final static String part3="]]></json-representation>" +
-   "<svg-representation><![CDATA[";
- final static String end="]]></svg-representation>" +
-   "</oryxmodel>";
- @Override
- public void doGet(HttpServletRequest req, HttpServletResponse res, Identity subject, Identity object)  {
+	final static String begin="<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+	"<oryxmodel>" +
+	"<description>";
+	final static String part1="</description>" +
+	"<type>";
+	final static String part2="</type>" +
+	"<json-representation><![CDATA[";
+	final static String part3="]]></json-representation>" +
+	"<svg-representation><![CDATA[";
+	final static String end="]]></svg-representation>" +
+	"</oryxmodel>";
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res, Identity subject, Identity object)  {
   
-  res.setContentType("text/xml");
-    res.setStatus(200);
-    try {
-     URL serverUrl = new URL( req.getScheme(),
-                    req.getServerName(),
+		res.setContentType("text/xml");
+		res.setStatus(200);
+		try {
+			URL serverUrl = new URL( req.getScheme(),
+					req.getServerName(),
                     req.getServerPort(),
                     "" );
-     res.setHeader("Content-Disposition", "inline; filename="+object.read().getTitle()+".xml" );
-     PrintWriter out = res.getWriter();
-     out.write(begin);
-     out.write(object.read().getSummary());
-     out.write(part1);
-     out.write(object.read().getType());
-     out.write(part2);
-     out.write(object.read().getJson(serverUrl.toString()));
-     out.write(part3);
-     out.write(object.read().getSvg());
-     out.write(end);
-    } catch (IOException ie) {
-   ie.printStackTrace();
-  }
-    }
+			res.setHeader("Content-Disposition", "inline; filename="+object.read().getTitle()+".xml" );
+			PrintWriter out = res.getWriter();
+			out.write(begin);
+			out.write(object.read().getSummary());
+			out.write(part1);
+			out.write(object.read().getType());
+			out.write(part2);
+			out.write(object.read().getJson(serverUrl.toString()));
+			out.write(part3);
+			out.write(object.read().getSvg());
+			out.write(end);
+		} 
+		catch (IOException ie) {
+			ie.printStackTrace();
+		}
+   	}
 }
