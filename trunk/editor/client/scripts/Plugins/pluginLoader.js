d877f96a613561b349e9c34d5a42c18d8314bdc0
philipp.berger@student.hpi.uni-potsdam.de <philipp.berger@student.hpi.uni-potsdam.de@d672c736-503d-0410-a38a-9366997c882b>
2010-04-22 20:52:40 +0000

    Merged new toolbar.js into trunk Note: the complete grouping of all will follow

--- trunk/editor/client/scripts/Plugins/pluginLoader.js
+++ trunk/editor/client/scripts/Plugins/pluginLoader.js
@@ -11,14 +11,15 @@ ORYX.Plugins.PluginLoader = Clazz.extend({
 		this.facade = facade;
 		
 		this.facade.offer({
-			'name': ORYX.I18N.PluginLoad.AddPluginButtonName,
-			'functionality': this.showManageDialog.bind(this),
-			'group': ORYX.I18N.SSExtensionLoader.group,
-			'icon': ORYX.PATH + "images/labs/script_add.png",
-			'description': ORYX.I18N.PluginLoad.AddPluginButtonDesc,
-			'index': 8,
-			'minShape': 0,
-			'maxShape': 0
+			name: ORYX.I18N.PluginLoad.AddPluginButtonName,
+			functionality: this.showManageDialog.bind(this),
+			group: ORYX.I18N.PluginLoad.group,
+            toolbarGroup: ORYX.I18N.PluginLoad.toolbarGroup,
+			icon: ORYX.PATH + "images/labs/script_add.png",
+			description: ORYX.I18N.PluginLoad.AddPluginButtonDesc,
+			index: 8,
+			minShape: 0,
+			maxShape: 0
 		});},
 	showManageDialog: function(){
 			this.mask = new Ext.LoadMask(Ext.getBody(), {msg:ORYX.I18N.Oryx.pleaseWait});
