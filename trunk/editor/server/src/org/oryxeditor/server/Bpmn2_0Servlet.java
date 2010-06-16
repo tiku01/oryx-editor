a34a1e36040f072f90259ad082fd7f1ab960133a
antoine.toulme <antoine.toulme@d672c736-503d-0410-a38a-9366997c882b>
2010-06-16 00:52:54 +0000

    fix for issue 435 missed parens

--- trunk/editor/server/src/org/oryxeditor/server/Bpmn2_0Servlet.java
+++ trunk/editor/server/src/org/oryxeditor/server/Bpmn2_0Servlet.java
@@ -56,122 +56,117 @@ import de.hpi.util.reflection.ClassFinder;
  * This servlet provides the access point to the interchange format of BPMN 2.0
  * 
  * @author Sven Wagner-Boysen
- *
+ * 
  */
 public class Bpmn2_0Servlet extends HttpServlet {
 
-	private static final long serialVersionUID = -4308758083419724953L;
-	
-	/**
-	 * The post request
-	 */
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException {
-		String json = req.getParameter("data");
-		boolean asXML = req.getParameter("xml") != null;
+    private static final long serialVersionUID = -4308758083419724953L;
 
-		/* Transform and return from DI */
-		try {
-			List<Class<? extends AbstractBpmnFactory>> factoryClasses = ClassFinder
-			.getClassesByPackageName(AbstractBpmnFactory.class,
-					"de.hpi.bpmn2_0.factory", this.getServletContext());
-			
-			StringWriter output = this.performTransformationToDi(json, asXML, factoryClasses);
-			res.setContentType("application/xml");
-			res.setStatus(200);
-			res.getWriter().print(output.toString());
-		} catch (Exception e) {
-			try {
-				e.printStackTrace();
-				res.setStatus(500);
-				res.setContentType("text/plain");
-				res.getWriter().write(e.getCause != null ? e.getCause().getMessage() : e.getMessage());
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
-		}
-		
-		
-		
-		
-	}
-	
-	/**
-	 * Triggers the transformation from Diagram to BPMN model and writes the 
-	 * resulting BPMN XML on success.
-	 * 
-	 * @param json
-	 * 		The diagram in JSON format
-	 * @param writer
-	 * 		The HTTP-response writer
-	 * @throws Exception
-	 * 		Exception occurred while processing
-	 */
-	protected StringWriter performTransformationToDi(String json, boolean asXML, List<Class<? extends AbstractBpmnFactory>> factoryClasses) throws Exception {
-		StringWriter writer = new StringWriter();
-		JSONObject result = new JSONObject();
-		
-		/* Retrieve diagram model from JSON */
-	
-		Diagram diagram = DiagramBuilder.parseJson(json);
-			
-		/* Build up BPMN 2.0 model */
-		Diagram2BpmnConverter converter = new Diagram2BpmnConverter(diagram, factoryClasses);
-		Definitions bpmnDefinitions = converter.getDefinitionsFromDiagram();
-		
-		/* Perform XML creation */
-		JAXBContext context = JAXBContext.newInstance(Definitions.class);
-		Marshaller marshaller = context.createMarshaller();
-		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-		
-		NamespacePrefixMapper nsp = new BPMNPrefixMapper();
-		marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", nsp);
-		
-		/* Set Schema validation properties */
-		SchemaFactory sf = SchemaFactory
-				.newInstance(javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI);
-		
-		String xsdPath = this.getServletContext().getRealPath("/WEB-INF/lib/bpmn20/BPMN20.xsd");
-		
-		Schema schema = sf.newSchema(new File(xsdPath));
-		marshaller.setSchema(schema);
-		
-		ExportValidationEventCollector vec = new ExportValidationEventCollector();
-		marshaller.setEventHandler(vec);
-		
-		/* Marshal BPMN 2.0 XML */
-		marshaller.marshal(bpmnDefinitions, writer);
-		
-		if(asXML) {
-			return writer;
-		}
-		
-		result.put("xml", writer.toString());
-		
-		/* Append XML Schema validation results */
-		if(vec.hasEvents()) {
-			ValidationEvent[] events = vec.getEvents();
-			StringBuilder builder = new StringBuilder();
-			builder.append("Validation Errors: <br /><br />");
-			
-			for(ValidationEvent event : Arrays.asList(events)) {
-				
-				builder.append("Line: ");
-				builder.append(event.getLocator().getLineNumber());
-				builder.append(" Column: ");
-				builder.append(event.getLocator().getColumnNumber());
-				
-				builder.append("<br />Error: ");
-				builder.append(event.getMessage());
-				builder.append("<br /><br />");
-			}
-			result.put("validationEvents", builder.toString());
-		}
-		
-		/* Prepare output */
-		writer = new StringWriter();
-		writer.write(result.toString());
-		
-		return writer;		
-	}
+    /**
+     * The post request
+     */
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException {
+        String json = req.getParameter("data");
+        boolean asXML = req.getParameter("xml") != null;
+
+        /* Transform and return from DI */
+        try {
+            List<Class<? extends AbstractBpmnFactory>> factoryClasses = ClassFinder.getClassesByPackageName(AbstractBpmnFactory.class,
+                    "de.hpi.bpmn2_0.factory", this.getServletContext());
+
+            StringWriter output = this.performTransformationToDi(json, asXML, factoryClasses);
+            res.setContentType("application/xml");
+            res.setStatus(200);
+            res.getWriter().print(output.toString());
+        } catch (Exception e) {
+            try {
+                e.printStackTrace();
+                res.setStatus(500);
+                res.setContentType("text/plain");
+                res.getWriter().write(e.getCause() != null ? e.getCause().getMessage() : e.getMessage());
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+
+    }
+
+    /**
+     * Triggers the transformation from Diagram to BPMN model and writes the
+     * resulting BPMN XML on success.
+     * 
+     * @param json
+     *            The diagram in JSON format
+     * @param writer
+     *            The HTTP-response writer
+     * @throws Exception
+     *             Exception occurred while processing
+     */
+    protected StringWriter performTransformationToDi(String json, boolean asXML, List<Class<? extends AbstractBpmnFactory>> factoryClasses) throws Exception {
+        StringWriter writer = new StringWriter();
+        JSONObject result = new JSONObject();
+
+        /* Retrieve diagram model from JSON */
+
+        Diagram diagram = DiagramBuilder.parseJson(json);
+
+        /* Build up BPMN 2.0 model */
+        Diagram2BpmnConverter converter = new Diagram2BpmnConverter(diagram, factoryClasses);
+        Definitions bpmnDefinitions = converter.getDefinitionsFromDiagram();
+
+        /* Perform XML creation */
+        JAXBContext context = JAXBContext.newInstance(Definitions.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        NamespacePrefixMapper nsp = new BPMNPrefixMapper();
+        marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", nsp);
+
+        /* Set Schema validation properties */
+        SchemaFactory sf = SchemaFactory.newInstance(javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+        String xsdPath = this.getServletContext().getRealPath("/WEB-INF/lib/bpmn20/BPMN20.xsd");
+
+        Schema schema = sf.newSchema(new File(xsdPath));
+        marshaller.setSchema(schema);
+
+        ExportValidationEventCollector vec = new ExportValidationEventCollector();
+        marshaller.setEventHandler(vec);
+
+        /* Marshal BPMN 2.0 XML */
+        marshaller.marshal(bpmnDefinitions, writer);
+
+        if (asXML) {
+            return writer;
+        }
+
+        result.put("xml", writer.toString());
+
+        /* Append XML Schema validation results */
+        if (vec.hasEvents()) {
+            ValidationEvent[] events = vec.getEvents();
+            StringBuilder builder = new StringBuilder();
+            builder.append("Validation Errors: <br /><br />");
+
+            for (ValidationEvent event : Arrays.asList(events)) {
+
+                builder.append("Line: ");
+                builder.append(event.getLocator().getLineNumber());
+                builder.append(" Column: ");
+                builder.append(event.getLocator().getColumnNumber());
+
+                builder.append("<br />Error: ");
+                builder.append(event.getMessage());
+                builder.append("<br /><br />");
+            }
+            result.put("validationEvents", builder.toString());
+        }
+
+        /* Prepare output */
+        writer = new StringWriter();
+        writer.write(result.toString());
+
+        return writer;
+    }
 
 }
