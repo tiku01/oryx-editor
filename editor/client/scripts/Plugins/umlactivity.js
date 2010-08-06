20995da621ea99ea32444f9922a052ab0abdd0da
ralf.diestelkaemper <ralf.diestelkaemper@d672c736-503d-0410-a38a-9366997c882b>
2010-08-06 19:57:18 +0000

    Zwischenstand vor der Abfahrt

--- editor/client/scripts/Plugins/umlactivity.js
+++ editor/client/scripts/Plugins/umlactivity.js
@@ -35,7 +35,7 @@
  * @extends Clazz
  * @param {Object} facade The facade of the editor
  */
- ORYX.Plugins.UMLUseCase = 
+ ORYX.Plugins.UMLActivity = 
 /** @lends ORYX.Plugins.UML.prototype */
 {
 	/**
@@ -47,190 +47,357 @@
 	 */
 	construct: function(facade) {
 		this.facade = facade;
-		this.facade.registerOnEvent(ORYX.CONFIG.EVENT_PROPWINDOW_PROP_CHANGED, this.handlePropertyChanged.bind(this));
-		this.facade.registerOnEvent('layout.uml.class', this.handleLayoutClass.bind(this));
-		this.facade.registerOnEvent('layout.uml.list', this.handleLayoutList.bind(this));
-		this.facade.registerOnEvent('layout.uml.association', this.handleLayoutAssociation.bind(this));
-		this.facade.registerOnEvent('layout.uml.qualified_association', this.handleLayoutQualifiedAssociation.bind(this));
-		this.facade.registerOnEvent(ORYX.CONFIG.EVENT_LOADED, this.addReadingDirectionOnLoad.bind(this));
-
+		this.facade.registerOnEvent('layout.uml.activityRegion', this.handleLayoutActivityRegion.bind(this));
 	},
 	
 	/**
-	 * Add reading direction on load.
-	 *
-	 * Because the reading direction arrow is added to the
-	 * label of an edge, we have to iterate over all edges
-	 * and add the arrow on load for it to appear.
-	 *
+	 * Handler for layouting event 'layout.bpmn2_0.pool'
 	 * @param {Object} event
 	 */
-	addReadingDirectionOnLoad : function(event) {
-		this.facade.getCanvas().edges.each(function(shape){
-			if (shape.properties["oryx-direction"] == "left" || shape.properties["oryx-direction"] == "right") {
-				this.addReadingDirection(shape);
+	handleLayoutActivityRegion: function(event){
+		
+		var pool = event.shape;
+		var selection = this.facade.getSelection(); 
+		var currentShape = selection.first();
+		
+		currentShape = currentShape || pool;
+		
+		this.currentPool = pool;
+		
+		// Check if it is a pool or a lane
+		if (!(currentShape.getStencil().id().endsWith("umlAcActivityRegion") || currentShape.getStencil().id().endsWith("ActivityRegionCanvas"))) {
+			return;
+		}
+		
+		if (!this.hashedBounds[pool.resourceId]) {
+			this.hashedBounds[pool.resourceId] = {};
+		}
+		
+		// Find all child lanes
+		var lanes = this.getLanes(pool);
+		
+		if (lanes.length <= 0) {
+			return
+		}
+		
+		// Show/hide caption regarding the number of lanes
+		if (lanes.length === 1 && this.getLanes(lanes.first()).length <= 0) {
+			// TRUE if there is a caption
+			lanes.first().setProperty("oryx-showcaption", lanes.first().properties["oryx-name"].trim().length > 0);
+			var rect = lanes.first().node.getElementsByTagName("rect");
+			rect[0].setAttributeNS(null, "display", "none");
+		} else {
+			lanes.invoke("setProperty", "oryx-showcaption", true);
+			lanes.each(function(lane){
+				var rect = lane.node.getElementsByTagName("rect");
+				rect[0].removeAttributeNS(null, "display");
+			})
+		}
+		
+		
+		
+		var allLanes = this.getLanes(pool, true);
+		
+		var deletedLanes = [];
+		var addedLanes = [];
+		
+		// Get all new lanes
+		var i=-1;
+		while (++i<allLanes.length) {
+			if (!this.hashedBounds[pool.resourceId][allLanes[i].resourceId]){
+				addedLanes.push(allLanes[i])
 			}
-		}.bind(this));
-	},
-	
-	/**
-	 * calculates the height of a text, taking line breaks into consideration
-	 *
-	 * @param {Object} labelElement the label
-	 * @param {String} the label test
-	 */
-	
-	calculateLabelHeight : function (labelElement, labelValue) {
-		var fontSize = labelElement.getFontSize();
-		var newlineOccurences = 1;
-		
-		labelValue.scan('\n', function() { newlineOccurences += 1; });
-		
-		// 0.75 account for padding around the label
-		return newlineOccurences * fontSize + 0.75;
-	},
-	
-	/**
-	 * Add Reading Direction to the name label after it has been changed
-	 */
-	handlePropertyChanged : function(event) {
-		if (event["key"] == "oryx-name") {
-			this.addReadingDirection(event.elements[0]);
 		}
-	},
-	
-	/**
-	 * Layout class shapes.
-	 *   - make text italic when abstract
-	 *   - resize methods and attributes boxes according to their content
-	 */
-	handleLayoutClass : function(event) {
-		var shape = event.shape;
 		
-		if (shape.propertiesChanged["oryx-abstract"] == true) {
-			var className = event.shape.getLabels().find(
-					function(label) { return label.id == (event.shape.id + "className") }
-				);
-			
-			if (shape.properties["oryx-abstract"] == true) {
-				className.node.setAttribute("font-style", "italic");
-			} else {
-				className.node.setAttribute("font-style", "none");
+		if (addedLanes.length > 0){
+			currentShape = addedLanes.first();
+		}
+		
+		
+		// Get all deleted lanes
+		var resourceIds = $H(this.hashedBounds[pool.resourceId]).keys();
+		var i=-1;
+		while (++i<resourceIds.length) {
+			if (!allLanes.any(function(lane){ return lane.resourceId == resourceIds[i]})){
+				deletedLanes.push(this.hashedBounds[pool.resourceId][resourceIds[i]]);
+				selection = selection.without(function(r){ return r.resourceId == resourceIds[i] });
 			}
+		}		
+				
+		var height, width;
+		
+		if (deletedLanes.length > 0 || addedLanes.length > 0) {
+			
+			// Set height from the pool
+			height = this.updateHeight(pool);
+			// Set width from the pool
+			width = this.adjustWidth(lanes, pool.bounds.width());	
+			
+			pool.update();
 		}
 		
-		if (shape.propertiesChanged["oryx-attributes"] == true || shape.propertiesChanged["oryx-methods"]) {
-			var attributesValue = event.shape.properties["oryx-attributes"];
-			var methodsValue = event.shape.properties["oryx-methods"];
-			var attributes = event.shape.getLabels().find(
-					function(label) { return label.id == (event.shape.id + "attributes") }
-				);
-			var methods = event.shape.getLabels().find(
-					function(label) { return label.id == (event.shape.id + "methods") }
-				);
-			var separator = event.shape._svgShapes.find(
-					function(element) { return element.element.id == (event.shape.id + "separator") }
-				).element;
-					
-					
-			var attributesHeight = this.calculateLabelHeight(attributes, attributesValue);
-			var methodsHeight = this.calculateLabelHeight(methods, methodsValue);
+		/**
+		 * Set width/height depending on the pool
+		 */
+		else if (pool == currentShape) {
 			
-			// 24px account for the class name
-			var distanceTilSeparator = 24 + attributesHeight + 2;
-			var distanceTilBottom = distanceTilSeparator + methodsHeight + 2;
-			
-			separator.setAttribute("y1", distanceTilSeparator);
-			separator.setAttribute("y2", distanceTilSeparator);
-			
-			// realign methods label (so that oryx' internal references are correct)
-			methods.y = distanceTilSeparator + 3;
-			methods.node.setAttribute("y", distanceTilSeparator + 3);
-			// realign the methods line by line (required for a visual change)
-			for (var i = 0; i < methods.node.childElementCount; i++) {
-				methods.node.childNodes[i].setAttribute("y", distanceTilSeparator + 2);
-			}
-			
-			// resize shape
-			shape.bounds.set(
-				shape.bounds.a.x, 
-				shape.bounds.a.y, 
-				shape.bounds.b.x, 
-				shape.bounds.a.y + distanceTilBottom + 5
-			);
+			// Set height from the pool
+			height = this.adjustHeight(lanes, undefined, pool.bounds.height());
+			// Set width from the pool
+			width = this.adjustWidth(lanes, pool.bounds.width());		
 		}
-	},
-	
-	/**
-	 * Layout the interface and enumeration shape. Resize according to their content.
-	 */
-	handleLayoutList : function(event) {
-		var shape = event.shape;
 		
-		if (shape.propertiesChanged["oryx-items"] == true) {
-			var itemsValue = shape.properties["oryx-items"];
-			var items = shape.getLabels().find(
-					function(label) { return label.id == (event.shape.id + "items") }
-				);
-			
-			var itemsHeight = this.calculateLabelHeight(items, itemsValue);
-		
-			var distanceTilBottom = 32 + itemsHeight + 2;
-			
-			// resize shape
-			shape.bounds.set(
-				shape.bounds.a.x, 
-				shape.bounds.a.y, 
-				shape.bounds.b.x, 
-				shape.bounds.a.y + distanceTilBottom + 5
-			);
-		}
-	},
-	
-	/**
-	 * Draws the reading direction arrow when an association is changed.
-	 */
-	handleLayoutAssociation : function(event) {
-		this.addReadingDirection(event.shape);
-	},
-	
-	/**
-	 * Adds the reading direction to the "name" label of an association.
-	 */ 
-	addReadingDirection : function(shape) {
-		var name = shape.getLabels().find(
-					function(label) { return label.id == (shape.id + "name") }
-				);
-		
-		if (shape.properties["oryx-direction"] == "left") {
-			name.text("◀ " + shape.properties["oryx-name"]);
-		}
-		else if (shape.properties["oryx-direction"] == "right") {
-			name.text(shape.properties["oryx-name"] + " ▶");
-		}
+		/**‚
+		 * Set width/height depending on containing lanes
+		 */		
 		else {
-			name.text(shape.properties["oryx-name"]);
+			// Get height and adjust child heights
+			height = this.adjustHeight(lanes, currentShape);
+			// Set width from the current shape
+			width = this.adjustWidth(lanes, currentShape.bounds.width()+(this.getDepth(currentShape,pool)*30));
 		}
 		
-		name.update();
+
+		this.setDimensions(pool, width, height);
+		
+		
+		
+		// Update all dockers
+		this.updateDockers(allLanes, pool);
+		
+		this.hashedBounds[pool.resourceId] = {};
+		
+		var i=-1;
+		while (++i < allLanes.length) {
+			// Cache positions
+			this.hashedBounds[pool.resourceId][allLanes[i].resourceId] = allLanes[i].absoluteBounds();
+			
+			this.hashedLaneDepth[allLanes[i].resourceId] = this.getDepth(allLanes[i], pool);
+			
+			this.forceToUpdateLane(allLanes[i]);
+		}
+		
+		this.hashedPoolPositions[pool.resourceId] = pool.bounds.clone();
+		
+		
+		// Update selection
+		//this.facade.setSelection(selection);		
+	},
+	forceToUpdateLane: function(lane){
+		
+		if (lane.bounds.height() !== lane._svgShapes[0].height) {	
+			lane.isChanged = true;
+			lane.isResized = true;
+			lane._update();
+		}
 	},
 	
+	getDepth: function(child, parent){
+		
+		var i=0;
+		while(child && child.parent && child !== parent){
+			child = child.parent;
+			++i
+		}
+		return i;
+	},
+	
+	updateDepth: function(lane, fromDepth, toDepth){
+		
+		var xOffset = (fromDepth - toDepth) * 30;
+		
+		lane.getChildNodes().each(function(shape){
+			shape.bounds.moveBy(xOffset, 0);
+			
+			[].concat(children[j].getIncomingShapes())
+					.concat(children[j].getOutgoingShapes())
+					
+		})
+		
+	},
+	
+	setDimensions: function(shape, width, height){
+		var isLane = shape.getStencil().id().endsWith("Lane");
+		// Set the bounds
+		shape.bounds.set(
+				isLane ? 30 : shape.bounds.a.x, 
+				shape.bounds.a.y, 
+				width	? shape.bounds.a.x + width - (isLane?30:0) : shape.bounds.b.x, 
+				height 	? shape.bounds.a.y + height : shape.bounds.b.y
+			);
+	},
+
+	setLanePosition: function(shape, y){
+		shape.bounds.moveTo(30, y);
+	},
+		
+	adjustWidth: function(lanes, width) {
+		
+		// Set width to each lane
+		(lanes||[]).each(function(lane){
+			this.setDimensions(lane, width);
+			this.adjustWidth(this.getLanes(lane), width-30);
+		}.bind(this));
+		
+		return width;
+	},
+	
+	
+	adjustHeight: function(lanes, changedLane, propagateHeight){
+		
+		var oldHeight = 0;
+		if (!changedLane && propagateHeight){
+			var i=-1;
+			while (++i<lanes.length){	
+				oldHeight += lanes[i].bounds.height();		
+			}
+		}
+		
+		var i=-1;
+		var height = 0;
+		
+		// Iterate trough every lane
+		while (++i<lanes.length){
+			
+			if (lanes[i] === changedLane) {
+				// Propagate new height down to the children
+				this.adjustHeight(this.getLanes(lanes[i]), undefined, lanes[i].bounds.height());
+				
+				lanes[i].bounds.set({x:30, y:height}, {x:lanes[i].bounds.width()+30, y:lanes[i].bounds.height()+height})
+								
+			} else if (!changedLane && propagateHeight) {
+				
+				var tempHeight = (lanes[i].bounds.height() * propagateHeight) / oldHeight;
+				// Propagate height
+				this.adjustHeight(this.getLanes(lanes[i]), undefined, tempHeight);
+				// Set height propotional to the propagated and old height
+				this.setDimensions(lanes[i], null, tempHeight);
+				this.setLanePosition(lanes[i], height);
+			} else {
+				// Get height from children
+				var tempHeight = this.adjustHeight(this.getLanes(lanes[i]), changedLane, propagateHeight);
+				if (!tempHeight) {
+					tempHeight = lanes[i].bounds.height();
+				}
+				this.setDimensions(lanes[i], null, tempHeight);
+				this.setLanePosition(lanes[i], height);
+			}
+			
+			height += lanes[i].bounds.height();
+		}
+		
+		return height;
+		
+	},
+	
+	
+	updateHeight: function(root){
+		
+		var lanes = this.getLanes(root);
+		
+		if (lanes.length == 0){
+			return root.bounds.height();
+		}
+		
+		var height = 0;
+		var i=-1;
+		while (++i < lanes.length) {
+			this.setLanePosition(lanes[i], height);
+			height += this.updateHeight(lanes[i]);
+		}
+		
+		this.setDimensions(root, null, height);
+		
+		return height;
+	},
+	
+	getOffset: function(lane, includePool, pool){
+		
+		var offset = {x:0,y:0};
+		
+		
+		/*var parent = lane; 
+		 while(parent) {
+		 				
+			
+			var offParent = this.hashedBounds[pool.resourceId][parent.resourceId] ||(includePool === true ? this.hashedPoolPositions[parent.resourceId] : undefined);
+			if (offParent){
+				var ul = parent.bounds.upperLeft();
+				var ulo = offParent.upperLeft();
+				offset.x += ul.x-ulo.x;
+				offset.y += ul.y-ulo.y;
+			}
+			
+			if (parent.getStencil().id().endsWith("Pool")) {
+				break;
+			}
+			
+			parent = parent.parent;
+		}	*/
+		
+		var offset = lane.absoluteXY();
+		
+		var hashed = this.hashedBounds[pool.resourceId][lane.resourceId] ||(includePool === true ? this.hashedPoolPositions[lane.resourceId] : undefined);
+		if (hashed) {
+			offset.x -= hashed.upperLeft().x; 	
+			offset.y -= hashed.upperLeft().y;		
+		} else {
+			return {x:0,y:0}
+		}		
+		return offset;
+	},
+	
+	getNextLane: function(shape){
+		while(shape && !shape.getStencil().id().endsWith("Lane")){
+			if (shape instanceof ORYX.Core.Canvas) {
+				return null;
+			}
+			shape = shape.parent;
+		}
+		return shape;
+	},
+	
+	getParentPool: function(shape){
+		while(shape && !shape.getStencil().id().endsWith("Pool")){
+			if (shape instanceof ORYX.Core.Canvas) {
+				return null;
+			}
+			shape = shape.parent;
+		}
+		return shape;
+	},
+	moveBy: function(pos, offset){
+		pos.x += offset.x;
+		pos.y += offset.y;
+		return pos;
+	},
+	
+	getHashedBounds: function(shape){
+		return this.currentPool && this.hashedBounds[this.currentPool.resourceId][shape.resourceId] ? this.hashedBounds[this.currentPool.resourceId][shape.resourceId] : shape.bounds.clone();
+	},
 	
 	/**
-	 * Resizes the qualifier box of a qualified association according to its content.
+	 * Returns a set on all child lanes for the given Shape. If recursive is TRUE, also indirect children will be returned (default is FALSE)
+	 * The set is sorted with first child the lowest y-coordinate and the last one the highest.
+	 * @param {ORYX.Core.Shape} shape
+	 * @param {boolean} recursive
 	 */
-	handleLayoutQualifiedAssociation : function(event) {
-		var shape = event.shape;
-		var qualifier = shape.getLabels().find(
-					function(label) { return label.id == (event.shape.id + "qualifier") }
-				);
-		
-		var size = qualifier._estimateTextWidth(shape.properties["oryx-qualifier"], 12);
-		// enforce minimum size, looks bad otherwise
-		if (size < 40) size = 40;
-		shape._markers.values()[0].element.lastElementChild.setAttribute("width", size+5);
-		shape._markers.values()[0].element.setAttributeNS(null, "markerWidth", size+5)
-	},
+	getLanes: function(shape, recursive){
+		var lanes = shape.getChildNodes(recursive||false).findAll(function(node) { return (node.getStencil().id() === "http://b3mn.org/stencilset/bpmn2.0#Lane"); });
+		lanes = lanes.sort(function(a, b){
+					// Get y coordinate
+					var ay = Math.round(a.bounds.upperLeft().y);
+					var by = Math.round(b.bounds.upperLeft().y);
+					
+					// If equal, than use the old one
+					if (ay == by) {
+						ay = Math.round(this.getHashedBounds(a).upperLeft().y);
+						by = Math.round(this.getHashedBounds(b).upperLeft().y);
+					}
+					return  ay < by ? -1 : (ay > by ? 1 : 0)
+				}.bind(this))
+		return lanes;
+	}
 };
 
-ORYX.Plugins.UML = Clazz.extend(ORYX.Plugins.UML);
+ORYX.Plugins.UMLActivity = Clazz.extend(ORYX.Plugins.UMLActivity);
