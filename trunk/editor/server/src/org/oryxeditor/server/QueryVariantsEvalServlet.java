6261dbf36ed3be3fbd2ab4cc001d18b38515aaa3
ahmad.gaafar@gmail.com <ahmad.gaafar@gmail.com@d672c736-503d-0410-a38a-9366997c882b>
2010-12-10 17:21:26 +0000

    Updates to the query variant evaluation servlet

--- trunk/editor/server/src/org/oryxeditor/server/QueryVariantsEvalServlet.java
+++ trunk/editor/server/src/org/oryxeditor/server/QueryVariantsEvalServlet.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2008, 2009 Steffen Ryll
+ * Copyright (c) 2009, 2010 Emilian Pascalau and Ahmed Awad
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,15 +21,11 @@
  */
 package org.oryxeditor.server;
 
-import java.io.ByteArrayInputStream;
+
 import java.io.IOException;
-import java.io.InputStream;
+
 import java.io.PrintWriter;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -39,43 +35,50 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 import com.bpmnq.AbstractQueryProcessor;
-import com.bpmnq.FileFormatException;
-import com.bpmnq.GraphBuilder;
-import com.bpmnq.Match;
 
 import com.bpmnq.ORYXModelXMLParser;
 import com.bpmnq.OryxMemoryQueryProcessor;
 import com.bpmnq.PartialProcessModel;
 import com.bpmnq.ProcessGraph;
-import com.bpmnq.QueryGraph;
-import com.bpmnq.QueryGraphBuilderRDF;
 import com.bpmnq.Utilities;
-import com.bpmnq.AbstractQueryProcessor.ProcessorCommand;
-import com.bpmnq.QueryGraphBuilderRDF.RdfSyntax;
-import com.bpmnq.compliancechecker.ComplianceViolationExplanator;
-import com.bpmnq.compliancechecker.ModelChecker;
-import com.bpmnq.compliancechecker.TemporalQueryGraph;
+
 
 public class QueryVariantsEvalServlet extends HttpServlet {
     private static final long serialVersionUID = -7946509291423453168L;
     private static final boolean useDataBaseConnection = false;
     private Logger log = Logger.getLogger(this.getClass());
-//    private Map<QueryGraph,String> queryMatches = new HashMap<QueryGraph,String>(); 
+ 
     /* (non-Javadoc)
      * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
      */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-    	System.out.print("QUERY VARIANT INVOKED");
+    	System.out.println("QUERY VARIANT INVOKED");
     	resp.setContentType("text/xml");
         resp.setCharacterEncoding("UTF-8");
         long startTime,endTime;
         startTime = System.currentTimeMillis();
         String rdf = req.getParameter("data");
         System.out.println("rdf is "+rdf);
+        // We have to process the incoming model rdf link
+        if (rdf.contains("bpmnqvar#"))
+        	rdf = rdf.replace("/oryx/editor;bpmnqvar#", "/backend/poem");
+        else if (rdf.contains("/oryx/editor;default?stencilset=/stencilsets/bpmnqvar/bpmnqvar.json#"))
+        	rdf = rdf.replace("/oryx/editor;default?stencilset=/stencilsets/bpmnqvar/bpmnqvar.json#", "/backend/poem");
+        else // this is fhe default# case
+        	rdf = rdf.replace("/oryx/editor;default#", "/backend/poem");
+        rdf=rdf+"/rdf";
         
-        rdf = rdf.replace("self", "rdf");
+        rdf = rdf.replace("//rdf", "/rdf");
+        
+        System.out.println("model link >>>> "+rdf);
+        
+        //rdf = rdf.replace("self", "rdf");
+        
+        
+        
+        
 //        InputStream rdfStream = new ByteArrayInputStream(rdf.getBytes("UTF-8"));
 //        log.debug("reading in rdfStream as UTF-8.");
 //        log.trace("read following RDF: " + rdf);
@@ -117,15 +120,7 @@ public class QueryVariantsEvalServlet extends HttpServlet {
    	    writer.println("</result>");
    	    writer.flush();
    	    System.out.println("Terminating PPM processing");
-   	    
-
-        
-        
-        
-        
-        
-   
-                
+   	                   
         try {
             Utilities.closeConnection();
         } catch (SQLException e) {
