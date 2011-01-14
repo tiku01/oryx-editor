09e5fb11e417b7e147d0c432338292120aa5b2ab
tobias.metzke@gmail.com <tobias.metzke@gmail.com@d672c736-503d-0410-a38a-9366997c882b>
2011-01-14 17:31:50 +0000

    UPA2010 Picture; every node has its picture now (PNG and SVG); nodes have property windows now, possible to hide/show them, other nodes auto-adjust to that; some bugs occur due to rowLayouting plug-in, need to be eliminated; next step: bugfixes and specific properties for nodes;

--- branches/upa2010-01/editor/client/scripts/Plugins/picturesupport.js
+++ branches/upa2010-01/editor/client/scripts/Plugins/picturesupport.js
@@ -62,16 +62,40 @@ ORYX.Plugins.PictureSupport = ORYX.Plugins.AbstractPlugin.extend({
         ORYX.CONFIG.SHAPEMENU_BUTTONS_PER_LEVEL_BOTTOM = 6;
         
         // catch occurring events
-		this.facade.registerOnEvent(ORYX.CONFIG.EVENT_LOADED, this.handleShape.bind(this));
+		this.facade.registerOnEvent(ORYX.CONFIG.EVENT_LOADED, this.pictureInstantiation.bind(this));
+		this.facade.registerOnEvent(ORYX.CONFIG.EVENT_PROPWINDOW_PROP_CHANGED, this.handleProperties.bind(this));
+		
  	},
  	
  	importPicture: function(){
  		
  	},
+	
+	handleProperties: function(event){
+		var shape = event.elements.first();
+		var properties = shape._svgShapes.find(function(element) { return element.element.id == (shape.id + "properties_frame") });
+		
+		if(event["key"]=="oryx-showproperties" && event["value"]==false){
+			shape.bounds.set(
+				shape.bounds.a.x, 
+				shape.bounds.a.y, 
+				shape.bounds.b.x, 
+				shape.bounds.a.y + 60);
+		};
+		
+		if(event["key"]=="oryx-showproperties" && event["value"]==true){
+			shape.bounds.set(
+				shape.bounds.a.x, 
+				shape.bounds.a.y, 
+				shape.bounds.b.x, 
+				shape.bounds.a.y + 60 + properties.height);
+			properties.y = 60;
+		};
+	},
  	
  	onSelectionChange: function(){},
  	
- 	handleShape: function(event){
+ 	pictureInstantiation: function(event){
  		//create a process lane if the canvas is empty
 		if(this.facade.getCanvas().children.length == 0){		
 			var mynamespace = "http://b3mn.org/stencilset/picture#";
