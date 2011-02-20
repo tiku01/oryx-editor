167c0edb416547ae4cd26f95f733385c26be24e6
tobias.metzke@gmail.com <tobias.metzke@gmail.com@d672c736-503d-0410-a38a-9366997c882b>
2011-02-20 18:16:25 +0000

    UPA2010 Picture; except the process child-tags all tags have equivalent classes now; need to think about methods for using the given structure of the XML in a good way now...

--- editor/server/src/de/hpi/pictureSupport/PictureBuildingBlockType.java
+++ editor/server/src/de/hpi/pictureSupport/PictureBuildingBlockType.java
@@ -1,5 +1,42 @@
 package de.hpi.pictureSupport;
 
+import org.xmappr.Attribute;
+import org.xmappr.Element;
+import org.xmappr.RootElement;
+
+@RootElement("resource")
 public class PictureBuildingBlockType {
 
+	@Attribute(name="id")
+	protected int ID;
+	
+	@Element(name="name")
+	protected String name;
+	
+	@Element(name="description")
+	protected String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
