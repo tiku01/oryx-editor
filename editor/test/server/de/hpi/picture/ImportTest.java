d2ef97e854809fabb73935323ffbcadc2f13fd29
meeti_m@freenet.de <meeti_m@freenet.de@d672c736-503d-0410-a38a-9366997c882b>
2011-03-23 19:23:24 +0000

    UPA2010 Picture; there was a minor change to the test of import, the JS and the importer I forgot to add;

--- editor/test/server/de/hpi/picture/ImportTest.java
+++ editor/test/server/de/hpi/picture/ImportTest.java
@@ -1,10 +1,8 @@
 package de.hpi.picture;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 
+import org.json.JSONException;
 import org.junit.Test;
 
 import de.hpi.pictureSupport.PictureConverter;
@@ -12,34 +10,7 @@ import de.hpi.pictureSupport.PictureConverter;
 public class ImportTest
 {
 	@Test
-	public void Importglobbox() throws IOException
-	{		
-		System.out.print(PictureConverter.importXML(this.getXMLNamed("process1.xml")));
+	public void Importglobbox() throws IOException, JSONException {		
+		System.out.print(PictureConverter.importXML(PictureConverter.getXMLNamed("process1.xml")));
 	}
-	
-	private String getXMLNamed(String filename) throws IOException
-	{
-		try
-		{
-			File f = new File(filename);
-
-			FileReader fReader = new FileReader(f);
-			BufferedReader bReader = new BufferedReader(fReader);
-			String xml = "";
-			while (bReader.ready())
-			{
-				xml += bReader.readLine();
-			}
-			
-			return xml;
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-			throw new IOException();
-		}
-		
-		
-	}
-
 }
